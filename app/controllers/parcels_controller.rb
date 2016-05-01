11ee1776bc8f6c6082fb229b76ad39ecc8c9784b
pudlovich <mac.stanisz@gmail.com>
2016-05-01 16:56:43 +0200

    simple parcel creation

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -7,11 +7,28 @@ class ParcelsController < ApplicationController
   end
 
   def new
+    @parcel = Parcel.new
   end
 
   def create
+    @parcel = Parcel.new(parcel_params)
+    if @parcel
+      if current_user
+        @parcel.sender = current_user
+      end
+      @parcel.save
+      redirect_to parcels_path, notice: 'DZIAŁA!'
+    else
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  private
+
+  def parcel_params
+    params.require(:parcel).permit(:weight, :width, :depth, :height, :price, :name)
   end
 end
