0d15e5d9a76101742a35ddb414dd53e84c849600
pudlovich <mac.stanisz@gmail.com>
2016-05-02 14:22:42 +0200

    rescue RecordNotFound error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
+
+  protected
+
+  def rescue_not_found
+    redirect_to :back, alert: t('errors.messages.record_not_found')
+    rescue ActionController::RedirectBackError
+      redirect_to root_path, alert: t('errors.messages.record_not_found')
+  end
 end
