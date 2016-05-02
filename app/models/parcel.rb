4a9edcb994bdf889bd73f35949e3e70647cc403a
pudlovich <mac.stanisz@gmail.com>
2016-05-02 13:15:24 +0200

    moved setting price to parcel model

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -6,4 +6,13 @@ class Parcel < ActiveRecord::Base
   validates :width, :height, :depth, :weight, :price, presence: true
   validates :weight, :price, numericality: { greater_than: 0 }
   validates :height, :depth, :width, numericality: { only_integer: true, greater_than: 0 }
+
+  before_validation :set_price, on: :create
+
+  private
+
+  def set_price
+    #dummy pricing function, to be substituted by actual business logic
+    self.price = 10
+  end
 end
