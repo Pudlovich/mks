3cb3fe0a45253b57c370ad34634a88affa9859ad
pudlovich <mac.stanisz@gmail.com>
2016-05-03 13:23:13 +0200

    created factories, test environment setup

---
+++ spec/factories/parcels.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :parcel do
+    width { Faker::Number.between(1, 100) }
+    height { Faker::Number.between(1, 100) }
+    depth { Faker::Number.between(1, 100) }
+    weight { Faker::Number.decimal(1,2) }
+
+    trait :with_name do
+      name { Faker::Commerce.product_name }
+    end
+
+    trait :with_sender do
+      sender { FactoryGirl.create(:user) }
+    end
+  end
+end
