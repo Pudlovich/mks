59e8b00580e8a356fd3c8c8622b0994746406dfa
pudlovich <mac.stanisz@gmail.com>
2016-05-14 15:58:59 +0200

    moved both delivery infos into the same spec file and described their behavior in a shared example

--- spec/models/delivery_infos_spec.rb
+++ spec/models/delivery_infos_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe RecipientInfo, type: :model do
+shared_examples 'a delivery address' do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:contact_name) }
   it { is_expected.to validate_presence_of(:zip_code) }
@@ -9,8 +9,15 @@ RSpec.describe RecipientInfo, type: :model do
   it { is_expected.to validate_presence_of(:phone_number) }
   it { is_expected.not_to validate_presence_of(:company_name) }
   it { is_expected.not_to validate_presence_of(:other_info) }
-
   it { is_expected.not_to allow_value('asdf').for(:email) }
+end
 
+RSpec.describe RecipientInfo, type: :model do
+  it_behaves_like 'a delivery address'
+  it { is_expected.to have_many(:parcels) }
+end
+
+RSpec.describe SenderInfo, type: :model do
+  it_behaves_like 'a delivery address'
   it { is_expected.to have_many(:parcels) }
 end
