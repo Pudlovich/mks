3fbfa6f4a315dda773f69ae9610c9af93e4aa3e8
pudlovich <mac.stanisz@gmail.com>
2016-05-22 14:18:17 +0200

    created spec for edit action

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -47,24 +47,42 @@ RSpec.describe Admin::UsersController do
   end
 
   describe "GET #edit" do
-    # let(:edited_user) { FactoryGirl.create(:user) }
-    # context "when user is a client" do
-    #   let(:user) { FactoryGirl.create(:user) }
-    #   it "redirects to applicatioon root" do
-    #     sign_in user
-    #     get :edit, id: edited_user.id
-    #     expect(response).to redirect_to root_path
-    #   end
-    # end
+    let(:edited_user) { FactoryGirl.create(:user) }
 
-    # context "when user is an employee" do
-    #   let(:user) { FactoryGirl.create(:user, :employee) }
-    #   it "redirects to applicatioon root" do
-    #     sign_in user
-    #     get :edit, id: edited_user.id
-    #     expect(response).to redirect_to root_path
-    #   end
-    # end
+    context "when user is a client" do
+      let(:user) { FactoryGirl.create(:user) }
+      it "redirects to applicatioon root" do
+        sign_in user
+        get :edit, id: edited_user
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "when user is an employee" do
+      let(:user) { FactoryGirl.create(:user, :employee) }
+      it "redirects to applicatioon root" do
+        sign_in user
+        get :edit, id: edited_user
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "when user is admin" do
+      let(:user) { FactoryGirl.create(:user, :admin) }
+
+      before(:each) do
+        sign_in user
+        get :edit, id: edited_user
+      end
+
+      it "assigns the requested user to @user" do
+        expect(assigns(:user)).to eq(edited_user)
+      end
+
+      it "renders the :edit view" do
+        expect(response).to render_template :edit
+      end
+    end
   end
 
   describe "PUT #update" do
