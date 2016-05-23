29623c1824da4b6653c0575b64bd8aa6fc6206aa
pudlovich <mac.stanisz@gmail.com>
2016-05-23 22:53:17 +0200

    added empty specs for the controller actions

---
+++ spec/controllers/employee/parcels_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe Employee::ParcelsController do
+  describe "GET #index" do
+
+  end
+
+  describe "GET #edit" do
+
+  end
+
+  describe "PUT #update" do
+
+  end
+end
