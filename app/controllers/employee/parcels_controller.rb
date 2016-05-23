4b79c6f29dfe4a9c64c9e4e19f010b011ea1e130
pudlovich <mac.stanisz@gmail.com>
2016-05-23 22:54:32 +0200

    added a employee:parcelscontroller with required actions

---
+++ app/controllers/employee/parcels_controller.rb
@@ -0,0 +1,20 @@
+class Employee::ParcelsController < EmployeeController
+
+  def index
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def parcel_params
+    
+  end
+end
