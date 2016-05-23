47ca335d5b8ee0829faf1c5dc16ffa6deae69680
pudlovich <mac.stanisz@gmail.com>
2016-05-23 23:31:17 +0200

    added acceptance enum to parcel model and tests for it working properly

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -3,6 +3,12 @@ class Parcel < ActiveRecord::Base
   belongs_to :sender_info
   belongs_to :recipient_info
 
+  enum acceptance: {
+    awaiting: 0,
+    accepted: 1,
+    rejected: 2
+  }
+
   delegate :city, to: :recipient_info, prefix: :recipient
 
   accepts_nested_attributes_for :sender_info
@@ -10,7 +16,7 @@ class Parcel < ActiveRecord::Base
 
   attr_localized :price, :weight
 
-  validates :width, :height, :depth, :weight, :price, :parcel_number, :sender_info, :recipient_info, presence: true
+  validates :width, :height, :depth, :weight, :price, :parcel_number, :sender_info, :recipient_info, :acceptance, presence: true
   validates :weight, :price, numericality: { greater_than: 0 }
   validates :height, :depth, :width, numericality: { only_integer: true, greater_than: 0 }
   validates :parcel_number, uniqueness: true
