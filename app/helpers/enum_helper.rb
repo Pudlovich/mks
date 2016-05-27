ff9f7bf4fd4380616617f465991fa04076c1e654
pudlovich <mac.stanisz@gmail.com>
2016-05-27 22:00:59 +0200

    changed name of the metod for translating enums for better readability

--- app/helpers/enum_helper.rb
+++ app/helpers/enum_helper.rb
@@ -1,11 +1,11 @@
 module EnumHelper
 
-  def t_enum(instance, enum)
+  def translate_enum(instance, enum)
     value = instance.send(enum);
-    t_enum_class(instance.class, enum, value)
+    translate_enum_class(instance.class, enum, value)
   end
 
-  def t_enum_class(class_name, enum, value)
+  def translate_enum_class(class_name, enum, value)
     unless value.blank?
       I18n.t("activerecord.enums.#{class_name.to_s.demodulize.underscore}.#{enum}.#{value}")
     end
