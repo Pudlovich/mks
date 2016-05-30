19120000155c32baded7cb483a4fb20d687de435
pudlovich <mac.stanisz@gmail.com>
2016-05-30 22:52:35 +0200

    created operations table in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521194430) do
+ActiveRecord::Schema.define(version: 20160530202136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "operations", force: :cascade do |t|
+    t.string   "place"
+    t.string   "additional_info"
+    t.integer  "kind",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "user_id",         null: false
+    t.integer  "parcel_id",       null: false
+  end
+
+  add_index "operations", ["parcel_id"], name: "index_operations_on_parcel_id", using: :btree
+  add_index "operations", ["user_id"], name: "index_operations_on_user_id", using: :btree
+
   create_table "parcels", force: :cascade do |t|
     t.string   "name"
-    t.integer  "width",                                                null: false
-    t.integer  "height",                                               null: false
-    t.integer  "depth",                                                null: false
-    t.decimal  "weight",                       precision: 6, scale: 2, null: false
-    t.decimal  "price",                        precision: 6, scale: 2, null: false
-    t.datetime "created_at",                                           null: false
-    t.datetime "updated_at",                                           null: false
+    t.integer  "width",                                                            null: false
+    t.integer  "height",                                                           null: false
+    t.integer  "depth",                                                            null: false
+    t.decimal  "weight",                       precision: 6, scale: 2,             null: false
+    t.decimal  "price",                        precision: 6, scale: 2,             null: false
+    t.datetime "created_at",                                                       null: false
+    t.datetime "updated_at",                                                       null: false
     t.integer  "sender_id"
-    t.string   "parcel_number",     limit: 20,                         null: false
-    t.integer  "sender_info_id",                                       null: false
-    t.integer  "recipient_info_id",                                    null: false
+    t.string   "parcel_number",     limit: 20,                                     null: false
+    t.integer  "sender_info_id",                                                   null: false
+    t.integer  "recipient_info_id",                                                null: false
+    t.integer  "acceptance_status",                                    default: 0, null: false
   end
 
   add_index "parcels", ["parcel_number"], name: "index_parcels_on_parcel_number", unique: true, using: :btree
@@ -84,6 +98,8 @@ ActiveRecord::Schema.define(version: 20160521194430) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "operations", "parcels"
+  add_foreign_key "operations", "users"
   add_foreign_key "parcels", "recipient_infos"
   add_foreign_key "parcels", "sender_infos"
   add_foreign_key "parcels", "users", column: "sender_id"
