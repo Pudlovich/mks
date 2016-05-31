b9d83bf3f919062ba8d4f70f38b77ae8efc280cd
pudlovich <mac.stanisz@gmail.com>
2016-05-31 18:47:14 +0200

    dummied out edit and update actions, as they are needless without actions to be made

--- app/controllers/employee/parcels_controller.rb
+++ app/controllers/employee/parcels_controller.rb
@@ -4,19 +4,19 @@ class Employee::ParcelsController < EmployeeController
     @parcels = Parcel.all
   end
 
-  def edit
-    @parcel = Parcel.find(params[:id])
-  end
+  # def edit
+  #   @parcel = Parcel.find(params[:id])
+  # end
 
-  def update
-    parcel = Parcel.find(params[:id])
-    parcel.update!(parcel_params)
-    redirect_to action: "index"
-  end
+  # def update
+  #   parcel = Parcel.find(params[:id])
+  #   parcel.update!(parcel_params)
+  #   redirect_to action: "index"
+  # end
 
-  private
+  # private
 
-  def parcel_params
-    params.require(:parcel)
-  end
+  # def parcel_params
+  #   params.require(:parcel)
+  # end
 end
