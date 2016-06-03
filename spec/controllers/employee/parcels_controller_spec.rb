1b1f766378ee5af03197f85cc95534e139ed1c25
pudlovich <mac.stanisz@gmail.com>
2016-06-03 21:42:47 +0200

    uncommented get edit controller tests

--- spec/controllers/employee/parcels_controller_spec.rb
+++ spec/controllers/employee/parcels_controller_spec.rb
@@ -54,52 +54,52 @@ RSpec.describe Employee::ParcelsController do
     end
   end
 
-  # describe "GET #edit" do
-  #   let(:edited_parcel) { FactoryGirl.create(:parcel) }
+  describe "GET #edit" do
+    let(:edited_parcel) { FactoryGirl.create(:parcel) }
 
-  #   context "when user is a client" do
-  #     let(:user) { FactoryGirl.create(:user) }
-  #     it "redirects to applicatioon root" do
-  #       sign_in user
-  #       get :edit, id: edited_parcel
-  #       expect(response).to redirect_to root_path
-  #     end
-  #   end
+    context "when user is a client" do
+      let(:user) { FactoryGirl.create(:user) }
+      it "redirects to applicatioon root" do
+        sign_in user
+        get :edit, id: edited_parcel
+        expect(response).to redirect_to root_path
+      end
+    end
 
-  #   context "when user is an employee" do
-  #     let(:user) { FactoryGirl.create(:user, :employee) }
+    context "when user is an employee" do
+      let(:user) { FactoryGirl.create(:user, :employee) }
 
-  #     before(:each) do
-  #       sign_in user
-  #       get :edit, id: edited_parcel
-  #     end
+      before(:each) do
+        sign_in user
+        get :edit, id: edited_parcel
+      end
 
-  #     it "assigns the requested parcel to @parcel" do
-  #       expect(assigns(:parcel)).to eq(edited_parcel)
-  #     end
+      it "assigns the requested parcel to @parcel" do
+        expect(assigns(:parcel)).to eq(edited_parcel)
+      end
 
-  #     it "renders the :edit view" do
-  #       expect(response).to render_template :edit
-  #     end
-  #   end
+      it "renders the :edit view" do
+        expect(response).to render_template :edit
+      end
+    end
 
-  #   context "when user is admin" do
-  #     let(:user) { FactoryGirl.create(:user, :admin) }
+    context "when user is admin" do
+      let(:user) { FactoryGirl.create(:user, :admin) }
 
-  #     before(:each) do
-  #       sign_in user
-  #       get :edit, id: edited_parcel
-  #     end
+      before(:each) do
+        sign_in user
+        get :edit, id: edited_parcel
+      end
 
-  #     it "assigns the requested parcel to @parcel" do
-  #       expect(assigns(:parcel)).to eq(edited_parcel)
-  #     end
+      it "assigns the requested parcel to @parcel" do
+        expect(assigns(:parcel)).to eq(edited_parcel)
+      end
 
-  #     it "renders the :edit view" do
-  #       expect(response).to render_template :edit
-  #     end
-  #   end
-  # end
+      it "renders the :edit view" do
+        expect(response).to render_template :edit
+      end
+    end
+  end
 
   # describe "PUT #update" do
 
