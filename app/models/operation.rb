1ec26ea51901ebbb561a9d41f29d47f1216ae04f
pudlovich <mac.stanisz@gmail.com>
2016-06-05 14:51:19 +0200

    added acceptance scope for finding operations dealing with acceptance status changes

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -5,6 +5,7 @@ class Operation < ActiveRecord::Base
   validates :kind, :parcel, presence: true
 
   scope :newest_first, -> { order(created_at: :desc) }
+  scope :acceptance, -> { where(kind: [1, 2]) }
 
   enum kind: {
     order_created: 0,
