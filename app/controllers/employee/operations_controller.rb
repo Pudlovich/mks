2aa3466ec64e8b1c21049a7a42123f8dea90ac5b
pudlovich <mac.stanisz@gmail.com>
2016-06-06 23:18:13 +0200

    first, primitive rendition of employee operations controller

--- app/controllers/employee/operations_controller.rb
+++ app/controllers/employee/operations_controller.rb
@@ -5,12 +5,18 @@ class Employee::OperationsController < EmployeeController
   end
 
   def create
-
+    @operation = Operation.new(operation_params)
+    @operation.user = current_user if user_signed_in?
+    if @operation.place.present? && @operation.save?
+      redirect_to new_employee_operation_path, notice: t('.operation_created_succesfully')
+    else
+      render 'new'
+    end
   end
 
   private
 
   def operation_params
-    params.require(:operation)
+    params.require(:operation).permit(:kind, :place, :additional_info)
   end
 end
