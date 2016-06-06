c45b0874e1b396324338b83bef6850254c275ccb
pudlovich <mac.stanisz@gmail.com>
2016-06-06 23:19:14 +0200

    empty spec for employee operations controller - its hard to make it work without settling on some implementation details

--- spec/controllers/employee/operations_controller_spec.rb
+++ spec/controllers/employee/operations_controller_spec.rb
@@ -41,6 +41,65 @@ RSpec.describe Employee::OperationsController do
   end
 
   describe "POST #create" do
+    before(:each) do 
+      sign_in user
+    end
 
+    context 'when user is a client' do
+      let(:user) { FactoryGirl.create(:user) }
+
+      it "redirects to applicatioon root" do
+        
+      end
+    end
+
+    context 'when user is an employee' do
+      let(:user) { FactoryGirl.create(:user, :employee) }
+
+      context 'valid attributes' do
+        it "saves a new Operation in the DB" do
+
+        end
+
+        it "assigns the correct parcel to the operation" do
+
+        end
+
+        it "redirects to the :new view" do
+          
+        end
+
+        context 'with additional_info' do
+          it "saves a new Operation in the DB" do
+
+          end
+
+          it "assigns the correct parcel to the operation" do
+
+          end
+
+          it "redirects to the :new view" do
+            
+          end
+        end
+      end
+
+      context 'no place given' do
+        it "doesn't save a new Operation in the DB" do
+
+        end
+
+        it "renders the :new view" do
+          
+        end
+      end
+    end
+
+    # is it necessary?
+    # context 'when user is admin' do
+    #   let(:user) { FactoryGirl.create(:user, :admin) }
+
+      
+    # end
   end
 end
