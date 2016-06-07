38cf441d167ef1225f8c2d1f45ec9b3624430af2
pudlovich <mac.stanisz@gmail.com>
2016-06-07 11:05:38 +0200

    settled on params structure for operations_controller

--- app/controllers/employee/operations_controller.rb
+++ app/controllers/employee/operations_controller.rb
@@ -5,8 +5,7 @@ class Employee::OperationsController < EmployeeController
   end
 
   def create
-    @operation = Operation.new(operation_params)
-    @operation.user = current_user if user_signed_in?
+    @operation = Operation.new(operation_params, user: current_employee)
     if @operation.place.present? && @operation.save?
       redirect_to new_employee_operation_path, notice: t('.operation_created_succesfully')
     else
@@ -18,5 +17,9 @@ class Employee::OperationsController < EmployeeController
 
   def operation_params
     params.require(:operation).permit(:kind, :place, :additional_info)
+  end
+
+  def parcel_number
+    params.require(:parcel_number)
   end
 end
