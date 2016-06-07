909d88d2fc20596c3d92dcd62456870fdf6c9538
pudlovich <mac.stanisz@gmail.com>
2016-06-07 11:46:56 +0200

    added a helper for getting options for select for employee operations_new

--- app/helpers/enum_helper.rb
+++ app/helpers/enum_helper.rb
@@ -12,4 +12,11 @@ module EnumHelper
   def translate_kind(kind)
     I18n.t("activerecord.attributes.operation.kinds.#{kind}")
   end
+
+  def kinds_for_select
+    # adding operations is not concerned with first three operation types, so they're dropped from the select box
+    Operation.kinds.keys.drop(3).map do |kind|
+      [translate_kind(kind), kind]
+    end
+  end
 end
