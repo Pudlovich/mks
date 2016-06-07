f409de1db588eda7d071eb94cc6a89a55035a6d2
pudlovich <mac.stanisz@gmail.com>
2016-06-07 20:56:42 +0200

    removed rspec matcher that is no longer adequate

--- spec/models/operations_spec.rb
+++ spec/models/operations_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Operation, type: :model do
   it { is_expected.to validate_presence_of(:kind) }
   it { is_expected.to validate_presence_of(:parcel) }
   it { is_expected.not_to validate_presence_of(:user) }
-  it { is_expected.not_to validate_presence_of(:place) }
   it { is_expected.not_to validate_presence_of(:additional_info) }
 
   it { is_expected.to belong_to(:parcel) }
