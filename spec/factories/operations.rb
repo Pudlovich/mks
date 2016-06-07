6e4be32a4246a4494993986021b735cb213a3924
pudlovich <mac.stanisz@gmail.com>
2016-06-07 15:36:38 +0200

    updated the operation factory to include traits for all kinds and optional fields

--- spec/factories/operations.rb
+++ spec/factories/operations.rb
@@ -11,5 +11,38 @@ FactoryGirl.define do
       kind 'order_accepted'
       user { FactoryGirl.create(:user, :employee) }
     end
+
+    trait :parcel_picked_up do
+      kind 'parcel_picked_up'
+      user { FactoryGirl.create(:user, :employee) }
+    end
+
+    trait :parcel_in_sorting_facility do
+      kind 'parcel_in_sorting_facility'
+      user { FactoryGirl.create(:user, :employee) }
+    end
+
+    trait :parcel_in_transit do
+      kind 'parcel_in_transit'
+      user { FactoryGirl.create(:user, :employee) }
+    end
+
+    trait :parcel_in_delivery do
+      kind 'parcel_in_delivery'
+      user { FactoryGirl.create(:user, :employee) }
+    end
+
+    trait :parcel_delivered do
+      kind 'parcel_delivered'
+      user { FactoryGirl.create(:user, :employee) }
+    end
+
+    trait :with_place do
+      place { Faker::Address.city }
+    end
+
+    trait :with_additional_info do
+      additional_info { Faker::StarWars.quote }
+    end
   end
 end
