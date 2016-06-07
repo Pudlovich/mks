5403694ded7088d9e69d18220dd1a91a0725723a
pudlovich <mac.stanisz@gmail.com>
2016-06-07 21:19:50 +0200

    added specs for custom validator behavior to operations_spec.rb

--- spec/models/operations_spec.rb
+++ spec/models/operations_spec.rb
@@ -10,4 +10,88 @@ RSpec.describe Operation, type: :model do
   it { is_expected.to belong_to(:user) }
 
   it { should define_enum_for(:kind) }
+
+  describe 'place validations' do
+    let(:parcel) { FactoryGirl.create(:parcel) }
+    before(:each) do
+      parcel
+    end
+
+    it 'allows creation of order_created operation without a place' do
+      expect(parcel.operations.count).to eq(1)
+      expect(parcel.operations.first.place).to be_blank
+    end
+
+    it 'allows creation of order_accepted operation without a place' do
+      expect{
+        FactoryGirl.create(:operation, :order_accepted, parcel: parcel)
+      }.to change(Operation,:count).by(1)
+    end
+
+    it 'allows creation of order_rejected operation without a place' do
+      expect{
+        FactoryGirl.create(:operation, :order_rejected, parcel: parcel)
+      }.to change(Operation,:count).by(1)
+    end
+
+    it 'doesnt allow creation of parcel_picked_up operation without a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_picked_up, parcel: parcel)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'doesnt allow creation of parcel_in_sorting_facility operation without a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_in_sorting_facility, parcel: parcel)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'doesnt allow creation of parcel_in_transit operation without a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_in_transit, parcel: parcel)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'doesnt allow creation of parcel_in_delivery operation without a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_in_delivery, parcel: parcel)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'doesnt allow creation of parcel_delivered operation without a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_delivered, parcel: parcel)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'allows creation of parcel_picked_up operation with a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_picked_up, :with_place, parcel: parcel)
+      }.to change(Operation,:count).by(1)
+    end
+
+    it 'allows creation of parcel_in_sorting_facility operation with a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_in_sorting_facility, :with_place, parcel: parcel)
+      }.to change(Operation,:count).by(1)
+    end
+
+    it 'allows creation of parcel_in_transit operation with a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_in_transit, :with_place, parcel: parcel)
+      }.to change(Operation,:count).by(1)
+    end
+
+    it 'allows creation of parcel_in_delivery operation with a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_in_delivery, :with_place, parcel: parcel)
+      }.to change(Operation,:count).by(1)
+    end
+
+    it 'allows creation of parcel_delivered operation with a place' do
+      expect{
+        FactoryGirl.create(:operation, :parcel_delivered, :with_place, parcel: parcel)
+      }.to change(Operation,:count).by(1)
+    end
+  end
 end
