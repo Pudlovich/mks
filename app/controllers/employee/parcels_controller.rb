71c9ebfeb1646e1e3cf182919ae2814359b8e13f
pudlovich <mac.stanisz@gmail.com>
2016-06-10 23:33:13 +0200

    pagination for employee parcels index

--- app/controllers/employee/parcels_controller.rb
+++ app/controllers/employee/parcels_controller.rb
@@ -4,11 +4,11 @@ class Employee::ParcelsController < EmployeeController
     if parcel = Parcel.find_by(parcel_number: params[:parcel_number])
       redirect_to edit_employee_parcel_path(parcel)
     end
-    @pending_parcels = Parcel.pending.newest_first
-    @accepted_parcels = Parcel.accepted.newest_first
-    @rejected_parcels = Parcel.rejected.newest_first
-    @parcels_in_transit = Parcel.in_transit.newest_first
-    @delivered_parcels = Parcel.delivered.newest_first
+    @pending_parcels = Parcel.pending.newest_first.paginate(page: params[:pending], per_page: 10)
+    @accepted_parcels = Parcel.accepted.newest_first.paginate(page: params[:accepted], per_page: 10)
+    @rejected_parcels = Parcel.rejected.newest_first.paginate(page: params[:rejected], per_page: 10)
+    @parcels_in_transit = Parcel.in_transit.newest_first.paginate(page: params[:in_transit], per_page: 10)
+    @delivered_parcels = Parcel.delivered.newest_first.paginate(page: params[:delivered], per_page: 10)
   end
 
   def edit
