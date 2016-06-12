faab21ac8075cafb9518b6e38c2fa4f8adcc18c5
pudlovich <mac.stanisz@gmail.com>
2016-06-12 18:55:56 +0200

    added methods to mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,21 @@
 class OrderMailer < ApplicationMailer
+  def order_created_mail(parcel)
+    @parcel = parcel
+    mail(bcc: [@parcel.sender_info.email, @parcel.recipient_info.email], subject: 'TEMAT MAILA')
+  end
+
+  def parcel_sent_mail(parcel)
+    @parcel = parcel
+    mail(to: @parcel.recipient_info.email, subject: 'TEMAT MAILA')
+  end
+
+  def parcel_in_delivery_mail(parcel)
+    @parcel = parcel
+    mail(to: @parcel.recipient_info.email, subject: 'TEMAT MAILA')
+  end
+
+  def parcel_delivered_mail(parcel)
+    @parcel = parcel
+    mail(bcc: [@parcel.sender_info.email, @parcel.recipient_info.email], subject: 'TEMAT MAILA')
+  end
 end
